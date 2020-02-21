25ec6737216d11d82d90ce3bbfd559929ccee55c
Patrick Ruhkopf <patrickruhkopf@gmail.com>
2020-02-20 22:32:56 -0500

    feat: update sidebar txt

--- _build/src/build-templates.ts
+++ _build/src/build-templates.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs'
 
 function precompileTpl(name: string) {
   const content = fs.readFileSync(`${__dirname}/templates/${name}.handlebars`, 'utf-8')
-  const compiled = Handlebars.precompile(content);
+  const compiled = Handlebars.precompile(content, { knownHelpers: { 'each': true, 'breaklines': true }, knownHelpersOnly: true});
   fs.writeFileSync(`${__dirname}/../../_includes/hb-templates/${name}.hb.js`, compiled)
 }
 
